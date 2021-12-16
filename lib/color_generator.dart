89ccd198d55f3fdb7c1ef89956a7b6522204a52f
Hazem Mohamed <hazemshata200@gmail.com>
2021-12-17 01:03:49 +0200

    Fixed ux in color generator

--- lib/color_generator.dart
+++ lib/color_generator.dart
@@ -14,17 +14,27 @@ class _ColorGeneratorState extends State<ColorGenerator> {
   Color _color = randomColor();
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const NavigationDrawer(),
-        appBar: AppBar(title: const Text('Random Color Generator')),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
+      drawer: const NavigationDrawer(),
+      appBar: AppBar(title: const Text('Random Color Generator')),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          if (index == 0) {
             return Container(
                 child: drawColor(
                     color: _color = randomColor(),
-                    colorCode: _color.toString()));
-          },
-          scrollDirection: Axis.vertical,
-        ));
+                    colorCode: _color.toString(),
+                    id: 1));
+          } else {
+            return Container(
+                child: drawColor(
+                    color: _color = randomColor(),
+                    colorCode: _color.toString(),
+                    id: 0));
+          }
+        },
+        scrollDirection: Axis.vertical,
+      ),
+    );
   }
 }
 
@@ -34,28 +44,41 @@ Color randomColor() {
       _random.nextInt(256), _random.nextInt(256));
 }
 
-Widget drawColor({required Color color, required String colorCode}) {
-  return Container(
-      color: color,
-      height: 500,
-      width: 500,
-      child: Tooltip(
-        showDuration: const Duration(seconds: 1),
-        waitDuration: const Duration(milliseconds: 2),
-        message: colorCode,
-        child: Center(
-          child: Text(
-            'Tap and Hold for Color Code',
-            style: TextStyle(
-                color: ((0.299 * color.red) +
-                            (0.587 * color.green) +
-                            (0.114 * color.blue) >
-                        128
-                    ? Colors.black
-                    : Colors.white),
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
+Widget drawColor(
+    {required Color color, required String colorCode, required int id}) {
+  if (id == 0) {
+    return Container(
+        color: color,
+        height: 500,
+        width: 500,
+        child: Tooltip(
+          showDuration: const Duration(seconds: 1),
+          waitDuration: const Duration(milliseconds: 2),
+          message: colorCode,
+        ));
+  } else {
+    return Container(
+        color: color,
+        height: 500,
+        width: 500,
+        child: Tooltip(
+          showDuration: const Duration(seconds: 1),
+          waitDuration: const Duration(milliseconds: 2),
+          message: colorCode,
+          child: Center(
+            child: Text(
+              'Tap and Hold for Color Code',
+              style: TextStyle(
+                  color: ((0.299 * color.red) +
+                              (0.587 * color.green) +
+                              (0.114 * color.blue) >
+                          128
+                      ? Colors.black
+                      : Colors.white),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-      ));
+        ));
+  }
 }
